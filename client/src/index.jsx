cc8d1c527e00e6001e5fa5d27d72cdcc98b72af9
Maksim Zhukau <Maksim.Zhukau@itechart-group.com>
2018-07-27 10:06:31 +0300

    added form pass change

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import App from './components/App';
+import App from './App';
 import './index.sass';
+
 
 ReactDOM.render((
   <Router>
