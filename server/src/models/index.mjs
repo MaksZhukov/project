41b4416ade1855c1a225548c9ff8a7e590582129
Maksim Zhukau <Maksim.Zhukau@itechart-group.com>
2018-08-14 11:09:28 +0300

    two

--- server/src/models/index.mjs
+++ server/src/models/index.mjs
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 import config from 'config';
 import logger from '../common/helpers/winston/index.mjs';
 
+console.log(config);
+
 mongoose.connect(`${config.dataBase.url}/${config.dataBase.name}`, { useNewUrlParser: true }, (error) => {
   if (error) {
     logger.error(error);
