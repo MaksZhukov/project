60f4ded3bd7741ec4906f099de1407608d25e7b3
Maksim Zhukau <Maksim.Zhukau@itechart-group.com>
2018-08-15 15:36:24 +0300

    two

--- server/src/models/index.mjs
+++ server/src/models/index.mjs
@@ -2,12 +2,17 @@ import mongoose from 'mongoose';
 import config from 'config';
 import logger from '../common/helpers/winston/index.mjs';
 
-console.log(`${config.dataBase.url}/${config.dataBase.name}`);
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://admin:maks_zhukov_97@ds213759.mlab.com:13759/react_video_games', { useNewUrlParser: true }, (error) => {
+mongoose.connect(`${config.dataBase.url}/${config.dataBase.name}`, { useNewUrlParser: true }, (error) => {
   if (error) {
     logger.error(error);
   } else {
     logger.info(`connected to mongodb on database ${config.dataBase.name}`);
+    mongoose.connect(`${config.dataBase.url}/${config.dataBase.name}`, { useNewUrlParser: true }, (err) => {
+      if (error) {
+        logger.error(err);
+      } else {
+        logger.info(`connected to mongodb on database ${config.dataBase.name}`);
+      }
+    });
   }
 });
