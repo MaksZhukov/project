7c8ab25036a3523bbb9f752ef5ec21cc62724cb3
Maksim Zhukau <Maksim.Zhukau@itechart-group.com>
2018-08-15 15:48:10 +0300

    two

--- server/src/models/index.mjs
+++ server/src/models/index.mjs
@@ -2,19 +2,25 @@ import mongoose from 'mongoose';
 import config from 'config';
 import logger from '../common/helpers/winston/index.mjs';
 
-mongoose.connect(`${config.dataBase.url}/${config.dataBase.name}`, { useNewUrlParser: true }, (error) => {
-  if (error) {
-    logger.error(error);
-  } else {
-    logger.info(`connected to mongodb on database ${config.dataBase.name}`);
-    setTimeout(() => {
-      mongoose.connect(`${config.dataBase.url}/${config.dataBase.name}`, { useNewUrlParser: true }, (err) => {
-        if (error) {
-          logger.error(err);
-        } else {
-          logger.info(`connected to mongodb on database ${config.dataBase.name}`);
-        }
-      });
-    }, 1000);
-  }
-});
+
+const options = {
+  reconnectTries: 30, // Retry up to 30 times
+  reconnectInterval: 500, // Reconnect every 500ms
+  poolSize: 10, // Maintain up to 10 socket connections
+  // If not connected, return errors immediately rather than waiting for reconnect
+  bufferMaxEntries: 0,
+  useNewUrlParser: true,
+};
+
+const connectWithRetry = () => {
+  mongoose.connect(`${config.dataBase.url}/${config.dataBase.name}`, options)
+    .then(() => {
+      logger.info(`connected to mongodb on database ${config.dataBase.name}`);
+    })
+    .catch((error) => {
+      logger.error(error);
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+
+connectWithRetry();
